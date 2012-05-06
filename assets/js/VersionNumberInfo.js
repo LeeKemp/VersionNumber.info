20cadb7d2d00ed668c074ba7bd8b0467abc5750f
Lee Kemp <lee@kumkee.com>
2012-05-06 21:00:43 +1000

    Added error checking for Request IP Address checking

--- assets/js/VersionNumberInfo.js
+++ assets/js/VersionNumberInfo.js
@@ -69,18 +69,24 @@
 }
 
 function myIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	try {
+	    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+	    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 
-    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-    xmlhttp.send();
+	    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+	    xmlhttp.send();
 
-    hostipInfo = xmlhttp.responseText.split("\n");
+	    hostipInfo = xmlhttp.responseText.split("\n");
 
-    for (i=0; hostipInfo.length >= i; i++) {
-        ipAddress = hostipInfo[i].split(":");
-        if ( ipAddress[0] == "IP" ) return ipAddress[1];
-    }
+	    for (i=0; hostipInfo.length >= i; i++) {
+	        ipAddress = hostipInfo[i].split(":");
+	        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+	    }
 
-    return false;
+	    return false;
+	} catch (Error)
+	{
+		console.log(Error);
+		return "<span class=\"label label-warning\">Unable to detect</span>";
+	}
 }
